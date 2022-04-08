c159a7cd0adeee3cad65cbe5d7375c0da107e1bb
Leo Fabiano Alves <leoalves@cvccorp.com.br>
2022-04-08 18:37:55 -0300

    Add new client - POST

--- backend/src/main/java/io/leofalves/clientCrud/services/ClientService.java
+++ backend/src/main/java/io/leofalves/clientCrud/services/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.leofalves.clientCrud.dto.ClientDto;
 import io.leofalves.clientCrud.entities.Client;
@@ -19,19 +20,34 @@ public class ClientService {
 	@Autowired
 	ClientRepository clientRepository;
 
+	@Transactional(readOnly = true)
 	public List<Client> findAll() {
 		List<Client> list = clientRepository.findAll();
 		return list;
 	}
 
+	@Transactional(readOnly = true)
 	public Page<ClientDto> findAllPaged(PageRequest pageRequest) {
 		Page<Client> page = clientRepository.findAll(pageRequest);
 		return page.map(c -> new ClientDto(c));
 	}
 
+	@Transactional(readOnly = true)
 	public ClientDto findById(Long id) {
-		Optional<Client> obj = clientRepository.findById(id); 
-		Client client = obj.orElseThrow(() -> new ResourceNotFoundException("Client not found")); 
+		Optional<Client> obj = clientRepository.findById(id);
+		Client client = obj.orElseThrow(() -> new ResourceNotFoundException("Client not found"));
+		return new ClientDto(client);
+	}
+
+	@Transactional
+	public ClientDto insert(ClientDto dto) {
+		Client client = new Client();
+		client.setName(dto.getName());
+		client.setIncome(dto.getIncome());
+		client.setCpf(dto.getCpf());
+		client.setBirthDate(dto.getBirthDate());
+		client.setChildren(dto.getChildren());
+		client = clientRepository.save(client);
 		return new ClientDto(client);
 	}
 }
